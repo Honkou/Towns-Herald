fbf19b07226a354903c20746b07eb75e68e2babd
Honkou <filiphancke@gmail.com>
2023-05-29 21:13:45 +0200

    Fix black and break isort

--- tests/wrong.py
+++ tests/wrong.py
@@ -1,7 +1,11 @@
 """Typical file."""
+import signal
+
+import abc
 
 
-def bad_func(one_very_long_arg: int, second_very_long_arg: str, third_very_long_args: dict
+def bad_func(
+    one_very_long_arg: int, second_very_long_arg: str, third_very_long_args: dict
 ) -> None:
     """Very bad."""
     pass
