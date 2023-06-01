d8af5f69c49ff6ef3951c62ae01b31d54d32bfa5
Honkou <filiphancke@gmail.com>
2023-06-01 20:20:06 +0200

    Create fake bot fixture
    It should be suitable to use for all tests that check if a chat event is being properly handled

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,7 +1,12 @@
 """Configuration file for pytest."""
 import os
 
+import discord
+import discord.ext.commands as commands
+import discord.ext.test as dpytest
 import pytest
+import pytest_asyncio
+from handle_messages import Listeners
 
 
 @pytest.fixture()
@@ -13,3 +18,19 @@ def set_environment_variable():
     os.environ["DISCORD_TOKEN"] = "Test_token"
     yield
     os.environ.pop("DISCORD_TOKEN")
+
+
+@pytest_asyncio.fixture
+async def fake_bot():
+    """Create a fake bot for command testing."""
+    intents = discord.Intents.default()
+    intents.members = True
+    intents.message_content = True
+    bot = commands.Bot(command_prefix="!", intents=intents)
+    await bot._async_setup_hook()
+    await bot.add_cog(Listeners(bot))
+    dpytest.configure(bot)
+
+    yield bot
+
+    await dpytest.empty_queue()
