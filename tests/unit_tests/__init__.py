fcecf61b3ecb0064c563f73669dd714fabae3ade
Honkou <73638904+Honkou@users.noreply.github.com>
2023-09-21 22:16:16 +0200

    Fix Ruff errors for the tests directory

---
+++ tests/unit_tests/__init__.py
@@ -0,0 +1,1 @@
+"""Init file of the unit_tests directory."""
--- tests/unit_tests/test_main.py
+++ tests/unit_tests/test_main.py
@@ -1,24 +1,25 @@
 """File containing all unit tests for main.py file."""
 import pytest
 from main import get_token, run_service
+from pytest_mock import MockerFixture
 
 
 class TestGetToken:
     """A class for all get_token related tests."""
 
-    def test_get_proper_token(self, set_environment_variable) -> None:
+    @pytest.mark.usefixtures("_set_environment_variable")
+    def test_get_proper_token(self) -> None:
         """Check if get_token properly returns the token from environment."""
         assert get_token() == "Test_token"
 
     def test_error_when_token_missing(self) -> None:
-        """Check if the function properly raises KeyError when
-        there is no DISCORD_TOKEN key in the environment.
-        """
+        """Check if the function properly raises KeyError without the proper token."""
         with pytest.raises(KeyError):
             get_token()
 
 
-def test_run_service(mocker, set_environment_variable) -> None:
+@pytest.mark.usefixtures("_set_environment_variable")
+def test_run_service(mocker: MockerFixture) -> None:
     """Assert that bot.run is being properly called.
 
     To achieve that, make the load_dotenv() function and
